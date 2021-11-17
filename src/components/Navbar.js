02caf173263d623f40e4bc7c21e02a0318f36538
Raghav Sharma <raghavsharma@Raghavs-MacBook-Pro.local>
2021-11-17 14:02:51 -0800

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img src="/assets/icon-flopped.png" alt='Thumbs Up Harold'></img>
+            <p>Meme Generator</p>
+        </nav>
+    );
+}
+
