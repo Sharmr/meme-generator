7b20669525be0768cce5f469d7aca39dcacf6562
Raghav Sharma <raghavsharma@Raghavs-MacBook-Pro.local>
2021-11-19 14:28:10 -0800

    you can add text now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import Navbar from "./components/Navbar";
 import Meme from "./components/Meme";
+import data from "./memesData";
+import React from "react";
 
 export default function App() {
+
+    const random_meme = data.data.memes[Math.floor(Math.random()*data.data.memes.length)];
+    const [meme, updateMeme] = React.useState(random_meme);
+    const [form_data, updateFormData] = React.useState(() => {
+        let f = {};
+        for(let i = 0; i < meme.box_count; i++) {
+            f = {...f, [`Text ${i+1}`]: ""};
+        }
+        return f;
+    });
+
+    function newText(event) {
+        const {value, name} = event.target;
+        console.log(event.target);
+        updateFormData((oldData) => {
+            return {
+                ...oldData,
+                [name]: value
+            }
+        });
+        console.log(form_data);
+    }
+
+    function getMeme() {
+        updateMeme(data.data.memes[Math.floor(Math.random()*data.data.memes.length)]);
+    }
     return (
         <>
             <Navbar />
-            <Meme />
+            <Meme 
+                box_count={meme.box_count} 
+                clickHandler={getMeme}
+                url={meme.url}
+                width={meme.width}
+                height={meme.height}
+                form_data={form_data}
+                newText={newText}
+            />
         </>
     );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,26 +1,81 @@
-export default function Meme() {
+import React from 'react';
+import Draggable from 'react-draggable';
+
+export default function Meme(props) {
+    
+
     return (
         <div className="meme">
-            <Form />
-            <Image />
+            <Form 
+                box_count={props.box_count} 
+                form_data={props.form_data} 
+                clickHandler={props.clickHandler} 
+                newText={props.newText}/>
+            <Image 
+                url={props.url} 
+                width={props.width} 
+                height={props.height} 
+                form_data={props.form_data} 
+                box_count={props.box_count}/>
+            
+            <p>(You can drag around the text and place it as you like on the image)</p>
+            <button className="download-button"> download as PNG</button>
+            <button className="download-button"> download as JPEG</button>
+            
         </div>
     );
 }
 
-function Form() {
+
+function Form({box_count, form_data, clickHandler, newText}) {
+    console.log(form_data[0])
+    function getTextBoxes({box_count, newText}) {
+        let b = [];
+        for(let i = 1; i <= box_count; i++) {
+            b.push(<TextBox box_number={i} text={form_data[`Text ${i}`]} newText={newText} key={i}/>);
+        }
+        return b;
+    }
+    const boxes = getTextBoxes({box_count, newText});
     return (
-            <form className="form">
-                <input type='text' className='text-field' id="text-field-top" defaultValue='Top Text'></input>
-                <input type='text' className='text-field' id="text-field-bottom" defaultValue='Bottom Text'></input>
-                <input type='submit' id='submit-button' value='Generate Meme'></input>
-            </form>
+            <div className="form">
+                {boxes}
+                <input 
+                    type='submit' 
+                    id='submit-button' 
+                    value='Generate Meme'
+                    onClick = {clickHandler}
+                />
+            </div>
     );
 }
 
-function Image() {
+function TextBox({box_number, text, newText}) {
+    return (
+        <input 
+            type='text' 
+            className='text-field' 
+            name={`Text ${box_number}`}
+            onChange={newText}
+            value={text}
+         />
+    );
+}
+
+function Image(meme) {
+    let meme_text_list = MemeText(meme.form_data, meme.box_count);
     return (
         <div className="image">
-
+            <img src={meme.url} alt='meme' width={meme.width} height={meme.height}></img>
+            {meme_text_list}
         </div>
     );
+}
+
+function MemeText(form_data, box_count) {
+    let meme_text_list = [];
+    for(let i=0; i < box_count; i++) {
+        meme_text_list.push(<Draggable bounds="parent" defaultPosition={{x:0, y: i*100}}><h2 className='meme-text'>{form_data[`Text ${i+1}`]}</h2></Draggable>);
+    }
+    return meme_text_list;
 }
